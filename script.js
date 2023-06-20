4a7684280c5350d81e6a92dd741c8789f082d035
Dev Sai <63507117+devsai9@users.noreply.github.com>
2023-06-20 12:19:10 -0400

    Deleted repeating if statements

--- script.js
+++ script.js
@@ -26,65 +26,69 @@ function startTime() {
 
     var month = today.getMonth() + 1;
     var monthWord;
-    if (month == 1) {
-        monthWord = "January";
-    } 
-    if (month == 2) {
-        monthWord = "February";
-    }
-    if (month == 3) {
-        monthWord = "March";
-    }
-    if (month == 4) {
-        monthWord = "April";
-    }
-    if (month == 5) {
-        monthWord = "May";
-    }
-    if (month == 6) {
-        monthWord = "June";
-    }
-    if (month == 7) {
-        monthWord = "July";
-    }
-    if (month == 8) {
-        monthWord = "August";
-    }
-    if (month == 9) {
-        monthWord = "September";
-    }
-    if (month == 10) {
-        monthWord = "October";
-    }
-    if (month == 11) {
-        monthWord = "November";
-    }
-    if (month == 12) {
-        monthWord = "December";
+    switch (month) {
+        case (month == 1):
+            monthWord = "January";
+            break;
+        case (month == 2):
+            monthWord = "February";
+            break;
+        case (month == 3):
+            monthWord = "March";
+            break;
+        case (month == 4):
+            monthWord = "April";
+            break;
+        case (month == 5):
+            monthWord = "May";
+            break;
+        case (month == 6):
+            monthWord = "June";
+            break;
+        case (month == 7):
+            monthWord = "July";
+            break;
+        case (month == 8):
+            monthWord = "August";
+            break;
+        case (month == 9):
+            monthWord = "September";
+            break;
+        case (month == 10):
+            monthWord = "October";
+            break;
+        case (month == 11):
+            monthWord = "November";
+            break;
+        case (month == 12):
+            monthWord = "December";
+            break;
     }
 
     var day = today.getDay();
     var dayWord;
-    if (day == 1) {
-        dayWord = "Monday";
-    }
-    if (day == 2) {
-        dayWord = "Tuesday";
-    }
-    if (day == 3) {
-        dayWord = "Wednesday";
-    }
-    if (day == 4) {
-        dayWord = "Thursday";
-    }
-    if (day == 5) {
-        dayWord = "Friday";
-    }
-    if (day == 6) {
-        dayWord = "Saturday";
-    }
-    if (day == 7) {
-        dayWord = "Sunday";
+    switch (day) {
+        case (day == 1):
+            dayWord = "Monday";
+            break;
+        case (day == 2):
+            dayWord = "Tuesday";
+            break;
+        case (day == 3):
+            dayWord = "Wednesday";
+            break;
+        case (day == 4):
+            dayWord = "Thursday";
+            break;
+        case (day == 5):
+            dayWord = "Friday";
+            break;
+        case (day == 6):
+            dayWord = "Saturday";
+            break;
+        case (day == 7):
+            dayWord = "Sunday";
+            break;
     }
 
     document.getElementById('date-header').innerHTML = dayWord + " " + monthWord + " " + today.getDate() + ", " + today.getFullYear();
@@ -101,9 +105,9 @@ function checkTime(i) {
 document.getElementById('flexSwitchCheckDefault').addEventListener('change', function() {
     const today = new Date();
     if (document.getElementById('flexSwitchCheckDefault').checked == true) {
-        document.cookie = "hr-format=true; expires=Thu, 16 July 2122 12:00:00 UTC; path=/;";
+        document.cookie = "hr-format=true; path=/;";
     } else {
-        document.cookie = "hr-format=false; expires=Thu, 16 July 2122 12:00:00 UTC; path=/;";
+        document.cookie = "hr-format=false; path=/;";
     }
     console.log(document.cookie);
 });
